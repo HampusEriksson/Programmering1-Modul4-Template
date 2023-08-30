c20819b4363ace62b6135c79ef6e87bbe54fb6f9
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-08-30 10:58:41 +0200

    Update uppg2_more_functions.py

---
+++ uppg2_more_functions.py
@@ -0,0 +1,31 @@
+# Funktioner
+# Namn
+# Klass
+# Påbörjat datum
+# Fyll i nedanstående funktioner enligt beskrivning
+# Du får inte använda inbyggda funktionerna max, len och sum
+
+
+def calculate_mean(numbers: list):
+    # Funktionen ska räkna ut medelvärdet av listan numbers
+    pass
+
+
+def find_highest_number(numbers: list):
+    # Funktionen ska returnera det största numret av listan numbers
+    pass
+
+
+def count_words(text: str):
+    # Funktionen ska returnera antal tecken i strängen text
+    pass
+
+
+def remove_duplicates(numbers: list):
+    # Funktionen ska ta bort alla dubletter från listan numbers
+    pass
+
+
+def calculate_sum(numbers: list):
+    # Funktionen ska returnera summan av listan numbers
+    pass
